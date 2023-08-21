888ce642e7f3adef91213645431e73f468e80b13
supratick444 <110408800+supratick444@users.noreply.github.com>
2023-08-21 12:36:03 +0000

    yo

---
+++ JS_concepts/j1.js
@@ -0,0 +1,16 @@
+// print out function 
+console.log("sonu");
+
+//Pilliar 1 : Coersion and Conversion 
+
+const value1 = '5';
+const value2 = 9 ;
+
+let sum = value1 + value2;
+
+console.log(sum); 
+
+let sum2 = Number(value1)+value2;
+console.log(sum2);
+
+// explicit 
